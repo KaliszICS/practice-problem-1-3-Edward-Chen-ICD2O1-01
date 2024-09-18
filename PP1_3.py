55e94e2323a3b19ca1259d7c13d9b36d12fbba16
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-09-18 14:25:00 +0000

    hjkjol

--- PP1_3.py
+++ PP1_3.py
@@ -3,13 +3,20 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-
+  print("A")
+  print("* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *")
+  print("B")
+  print("* * * * *\n*\t*\n*\t*\n*\t*\n* * * * *")
+  print("C")
+  print("* * * * * *\n *\t *\n  *\t*\n   *   *\n    * *\n     *")
 def q2():
   #Write Assignment code here
+  print("Hello\nWorld")
   
 def q3():
   #Write Assignment code here
-  
+  print("Man"</br>)
+  print("Board"\br)
 
 
 #Do not alter anything after this line
