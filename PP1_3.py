c6ecdb3bc17b315193f77c61cdf5e8d91a359995
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-11-04 16:23:33 -0500

    Update PP1_3.py

--- PP1_3.py
+++ PP1_3.py
@@ -1,30 +1,7 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-print(
-    
-    " * * * * * *\n"
-    "* * * * * *\n"
-    " * * * * * *\n"
-    "* * * * * *\n"
-    " * * * * * *\n"
-    "* * * * * *\n\n"
-    
-    
-    "* * * * *\n"
-    "*       *\n"
-    "*       *\n"
-    "*       *\n"
-    "* * * * *\n\n"
-    
-    
-    "* * * * * *\n"
-    "\t*       *\n"
-    "\t  *     *\n"
-    "\t   *   *\n"
-    "\t    * *\n"
-    "\t     *"
-)
+print( " * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n\n* * * * *\n*       *\n*       *\n*       *\n* * * * *\n\n* * * * * *\n\t*       *\n\t  *     *\n\t   *   *\n\t    * *\n\t     *")
 def q2():
   #Write Assignment code here
   print("Hello " + "World")
